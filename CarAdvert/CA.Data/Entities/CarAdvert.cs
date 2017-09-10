eab87a0ca93e92ed6f6869450f8edf45292e4791
Oleksandr <a.mykhailovskyi@gmail.com>
2017-09-10 23:20:24 +0200

    Added Data project with needed entities

---
+++ CarAdvert/CA.Data/Entities/CarAdvert.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CA.Data.Entities
+{
+    public class CarAdvert
+    {
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Title of the advertisement.
+        /// </summary>
+        [Required]
+        [StringLength(64)]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Type id of fuel.
+        /// </summary>
+        [Required]
+        public int FuelTypeId { get; set; }
+
+        /// <summary>
+        /// The price of car.
+        /// </summary>
+        [Required]
+        public int Price { get; set; }
+
+        /// <summary>
+        /// Indicates if car is new or used.
+        /// </summary>
+        public bool New { get; set; }
+
+        /// <summary>
+        /// Number of mileages. For used car.
+        /// </summary>
+        public int Mileage { get; set; }
+
+        /// <summary>
+        /// Date of the first registration. For used car.
+        /// </summary>
+        public DateTime? FirstRegistration { get; set; }
+
+        
+        // Navigation properties
+
+        [ForeignKey("FuelTypeId")]
+        public virtual FuelType Fuel { get; set; }
+    }
+}
---
+++ CarAdvert/CA.Data/Entities/FuelType.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CA.Data.Entities
+{
+    public class FuelType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of fuel.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        //Navigation properties
+
+        public virtual ICollection<CarAdvert> Adverts { get; set; }
+    }
+}
